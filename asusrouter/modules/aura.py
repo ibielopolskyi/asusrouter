9caf0396c2b7e3ca29e402ccca431cd240587396
igor <igor@bielopolskyi.com>
2025-05-21 22:08:34 -0400

    OldEffect 0 edge case

--- asusrouter/modules/aura.py
+++ asusrouter/modules/aura.py
@@ -221,7 +221,7 @@ async def set_state(
         state = get_scheme_from_state(aura_state)
 
     # Check if the state has color support
-    if aura_state.get('oldeffect') and state.name not in AsusAuraSingleZoneColor.__members__:
+    if 'oldeffect' in state and state.name not in AsusAuraSingleZoneColor.__members__:
         return await callback(
             endpoint=EndpointControl.COMMAND,
             commands={"aurargb_enable": state.value, "action_mode": "apply", "rc_service": "start_aurargb"}
